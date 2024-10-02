0270851cd8ffdb0ee3e347d393648918a03bf88d
Jenna <jcwyman@uvm.edu>
2024-10-02 10:29:11 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// Object type: Dog
+
+// Properties
+    // Name: Kevin
+    // Breed: Corgi
+    // Age: 2 years
+// Events
+    // Eat food - Happens when hungry
+    // Takes a nap - Happens when tired
+    // Goes on a walk - Happens when energized
+// Methods
+    // Gets full - When fed
+    // Feels energized - When naps
+    // Feels happy - When walked
